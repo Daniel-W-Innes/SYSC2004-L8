4cc23a1d5ffe6b343d623dfdd0d9cd57ec5c1d42
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-11-12 13:14:55 -0500

    GoToJailSquare

--- Lab8/src/GoToJailSquare.java
+++ Lab8/src/GoToJailSquare.java
@@ -1,4 +1,15 @@
 
-public class GoToJailSquare {
+public class GoToJailSquare extends Square{
 
+	private Square jailSquare;
+	
+	GoToJailSquare(int number, Square jailSquare) {
+		super("Go To Jail", number);
+		this.jailSquare = jailSquare;
+	}
+
+	@Override
+	public void landOn(Player p){
+		p.setLocation(jailSquare);
+	}
 }
