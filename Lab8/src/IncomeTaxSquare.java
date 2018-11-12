ee7b14aa10da6be6398fa63ea2c3d8138bb81c52
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-11-12 13:05:53 -0500

    IncomeTaxSquare

--- Lab8/src/IncomeTaxSquare.java
+++ Lab8/src/IncomeTaxSquare.java
@@ -1,4 +1,15 @@
 
-public class IncomeTaxSquare {
+public class IncomeTaxSquare extends Square {
+	private int maximumTax;
 
+	IncomeTaxSquare(int maxTax) {
+		super("Income Tax", 0);
+		maximumTax = maxTax;
+	}
+
+	@Override
+	public void landOn(Player p) {
+		p.setLocation(this);
+		p.decreaseCash(p.netWorth() / 10 > maximumTax ? maximumTax : p.netWorth() / 10);
+	}
 }
