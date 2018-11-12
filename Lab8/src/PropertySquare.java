63d46621d1b122986349024b7af23ba0a945c807
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-11-12 13:35:33 -0500

    PropertySquare

--- Lab8/src/PropertySquare.java
+++ Lab8/src/PropertySquare.java
@@ -1,4 +1,29 @@
 
-public class PropertySquare {
+public class PropertySquare extends Square{
+	private int price;
+	private int rent;
+	private Player owner;
+	
+	PropertySquare(String name, int number, int price, int rent){
+		super(name, number);
+		this.price = price;
+		this.rent = rent;
+		owner = null;
+	}
 
+	@Override
+	public void landOn(Player p) throws Exception {
+		p.setLocation(this);
+		if(owner == null && p.netWorth() > price) {
+			owner = p;
+			p.decreaseCash(price);
+		}else if(owner != p && owner != null && p.netWorth() >= rent){
+			owner.increaseCash(rent);
+			p.decreaseCash(rent);
+		}else if(owner != p && owner != null && p.netWorth() < rent){
+			owner.increaseCash(p.netWorth());
+			p.decreaseCash(p.netWorth());
+			throw new Exception("There is no capitalism without debt, " + p.getName() + " is out");
+		}
+	}
 }
