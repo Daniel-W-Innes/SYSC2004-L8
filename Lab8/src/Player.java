e3ee03ccbb23849cfc579d2e3299ddf2d967cf95
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-11-12 12:46:34 -0500

    Player

--- Lab8/src/Player.java
+++ Lab8/src/Player.java
@@ -1,4 +1,51 @@
 
-public class Player {
-	
+public class Player implements Comparable<Object> {
+	private String name;
+	private Square location;
+	private int cash;
+
+	Player(String name, Square startSquare) {
+		this.name = name;
+		location = startSquare;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Square getLocation() {
+		return location;
+	}
+
+	public void setLocation(Square location) {
+		this.location = location;
+	}
+
+	public int getCash() {
+		return cash;
+	}
+
+	public int netWorth() {
+		return cash;
+	}
+
+	public void increaseCash(int amount) {
+		cash += amount;
+	}
+
+	public void decreaseCash(int amount) {
+		cash -= amount;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (((Player) o).netWorth() < netWorth()) {
+			return 1;
+		} else if (((Player) o).netWorth() > netWorth()) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
 }
