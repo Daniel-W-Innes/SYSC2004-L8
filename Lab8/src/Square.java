b470db7c6bcd048b53e474fcde6e0cac7b4c957f
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-11-12 12:15:43 -0500

    Square

---
+++ Lab8/src/Square.java
@@ -0,0 +1,20 @@
+
+public abstract class Square {
+	private String name;
+	private int number;
+	
+	Square(String name, int number){
+		this.name = name;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public abstract void landOn(Player p) throws Exception;
+}
